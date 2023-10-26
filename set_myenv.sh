2b11260bb657b4d9349e8b145aa813f776f8c621
Agnès Ferté <ferte@slac.stanford.edu>
2023-10-26 12:18:29 -0700

    Update set env.

--- set_myenv.sh
+++ set_myenv.sh
@@ -1,1 +1,4 @@
-export CSL_PATH=/path/to/cosmosis-standard-library
+export CSL_PATH=/path/to/your/cosmosis-standard-library
+export REPO_PATH=/path/to/where/you/cloned/tutorial_wlcosmology
+pip install astropy
+cd ini_inference
